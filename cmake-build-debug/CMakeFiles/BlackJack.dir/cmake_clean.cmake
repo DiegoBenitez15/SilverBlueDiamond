54398e955995257c37f4e146d491a1801cec60ff
GrimSkul1 <54964499+GrimSkul1@users.noreply.github.com>
2019-11-08 11:55:29 -0400

    Bryan´s Commit

---
+++ cmake-build-debug/CMakeFiles/BlackJack.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "BlackJack.exe"
+  "BlackJack.exe.manifest"
+  "BlackJack.pdb"
+  "CMakeFiles/BlackJack.dir/BlackJack/BlackJack.c.obj"
+  "CMakeFiles/BlackJack.dir/BlackJack/main.c.obj"
+  "libBlackJack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BlackJack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
