72315a2907d0ea43f0c9d8b044bce3620c820358
GrimSkul1 <54964499+GrimSkul1@users.noreply.github.com>
2019-12-04 22:27:13 -0400

    Bryan's Commit

--- Estetica/Main-Estetica.c
+++ Estetica/Main-Estetica.c
@@ -67,7 +67,7 @@ void bienvenido(){
 }
 
 void mesacraps(){
-    printf("%c",201);
+    printf("\t\t\t%c",201);
     for(int i = 0; i < 28; i++)
         printf("%c",205);
     printf("%c",203);
@@ -78,7 +78,7 @@ void mesacraps(){
     printf("\t\t\t%c   %c",186,218);
     for(int i = 0; i < 19; i++)
         printf("%c",196);
-    printf("\t\t\t%c    %c   %c6%c   %c\n",191,186,174,175,186);
+    printf("%c    %c   %c6%c   %c\n",191,186,174,175,186);
     printf("\t\t\t%c  %c%c3 %c 4 %c 9 %c 10 %c 11%c%c   %c  %c X 25 %c\n",186,174,179,250,250,250,250,179,175,186,36,186);
     printf("\t\t\t%c   %c",186,192);
     for(int i = 0; i < 19; i++)
@@ -86,37 +86,38 @@ void mesacraps(){
     printf("%c    %c",217,204);
     for(int i = 0; i < 9; i++)
         printf("%c",205);
-    printf("\t\t\t%c\n",185);
+    printf("%c\n",185);
     printf("\t\t\t%c\t %c %c  X  5 %c\t     %c    P    %c\n",186,179,36,179,186,186);
     printf("\t\t\t%c   %c     %c",186,245,218);
     for(int i = 0; i < 8; i++)
         printf("%c",196);
-    printf("\t\t\t%c\t %c   %c         %c\n",191,245,186,186);
+    printf("%c\t %c   %c         %c\n",191,245,186,186);
     printf("\t\t\t%c   %c    %c%c 2 %c 12 %c%c    %c   %c    A    %c\n",186,245,174,179,250,179,175,245,186,186);
     printf("\t\t\t%c   %c     %c",186,245,192);
     for(int i = 0; i < 8; i++)
         printf("%c",196);
-    printf("\t\t\t%c     %c   %c         %c\n",217,245,186,186);
+    printf("%c     %c   %c         %c\n",217,245,186,186);
     printf("\t\t\t%c   %c     %c%c  X  10%c     %c   %c    S    %c\n",186,245,179,36,179,245,186,186);
     printf("\t\t\t%c   %c                    %c   %c         %c\n",186,245,245,186,186);
     printf("\t\t\t%c\tC  A  M  P  O\t     %c \t  E    %c\n",186,186,186);
     printf("\t\t\t%c",204);
     for(int i = 0; i < 8; i++)
         printf("%c",205);
-    printf("\t\t\t%c",203);
+    printf("%c",203);
     for(int i = 0; i < 19; i++)
         printf("%c",205);
-    printf("\t\t\t%c\t       %c\n",188,186);
+    printf("%c\t       %c\n",188,186);
     printf("\t\t\t%c  BIG   %c L   I   N   E   A     D E   %c\n",186,186,186);
     printf("\t\t\t%c %c 8 %c  %c\t\t\t       %c\n",186,174,175,186,186);
     printf("\t\t\t%c %c X 25 %c  %c 7 %c 11 %c  = %c%c  X  10%c   %c\n",186,36,186,174,38,175,179,36,179,186);
     printf("\t\t\t%c",200);
     for(int i = 0; i < 8; i++)
         printf("%c",205);
-    printf("\t\t\t%c",202);
+    printf("%c",202);
     for(int i = 0; i < 29; i++)
         printf("%c",205);
-    printf("\t\t\t%c\n",188);
+    printf("%c\n",188);
+
 }
 
 void instrucciones_bj(){
